2bab19c393d5f962237baaf96818c322439a7a13
raja_choudhary <raja@choudhary.com>
2024-07-30 11:08:32 +0530

    now fixed quiz fetch

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -23,6 +23,8 @@ export const UserAuth = async(req)=>{
 //    const isVerified = jwt.verify(cookie.value,process.env.JWT_SECRET).password ===  process.env.ADMIN_PASSWORD
    const userId = getJWT(cookie.value).id;
 
+   console.log(userId);
+
    const user = await User.findById(userId);
 
    if(!user) return false;
